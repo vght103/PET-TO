95421379ff5358bd641566560a40dd9e5fabe4e6
vght103 <74849404+vght103@users.noreply.github.com>
2021-09-18 22:45:35 +0900

    content dateTime added

--- src/components/content_item/content_item.jsx
+++ src/components/content_item/content_item.jsx
@@ -5,17 +5,17 @@ import styles from "./content_item.module.css";
 
 const ContentItem = ({ item, isOwner }) => {
   const history = useHistory();
-  // const [createdTime, setCreatedTime] = useState(null);
+  const [createdTime, setCreatedTime] = useState(null);
 
-  // useEffect(() => {
-  //   const dateTime = item.createdAt.toDate();
-  //   const year = dateTime.getFullYear();
-  //   const month = String(dateTime.getMonth() + 1).padStart(2, "0");
-  //   const date = String(dateTime.getDate()).padStart(2, "0");
-  //   const hours = String(dateTime.getHours()).padStart(2, "0");
-  //   const minutes = String(dateTime.getMinutes()).padStart(2, "0");
-  //   setCreatedTime(`${year}.${month}.${date} ${hours}:${minutes}`);
-  // }, [item]);
+  useEffect(() => {
+    const dateTime = item.createdAt.toDate();
+    const year = dateTime.getFullYear();
+    const month = String(dateTime.getMonth() + 1).padStart(2, "0");
+    const date = String(dateTime.getDate()).padStart(2, "0");
+    const hours = String(dateTime.getHours()).padStart(2, "0");
+    const minutes = String(dateTime.getMinutes()).padStart(2, "0");
+    setCreatedTime(`${year}.${month}.${date} ${hours}:${minutes}`);
+  }, [item]);
 
   const goToContentInfo = () => {
     history.push({
@@ -29,7 +29,7 @@ const ContentItem = ({ item, isOwner }) => {
       <div className={styles.content_wrap}>
         <div className={styles.content_info}>
           <span className={styles.category}>{item.category}</span>
-          {/* <span>{createdTime}</span> */}
+          <span>{createdTime}</span>
           <p className={styles.content_text}>{item.contentText}</p>
         </div>
         {item.imgFilesUrl && (
